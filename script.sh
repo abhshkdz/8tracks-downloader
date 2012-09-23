7cfb448a8047b9be5a4b4a72af370526c06437f2
Abhishek Das <das.abhshk@gmail.com>
2012-09-23 06:29:35 +0530

    Beautiful file naming song|artist|album.m4a

--- script.sh
+++ script.sh
@@ -16,12 +16,14 @@ for ((i=1; i<$number_of_tracks; i++)); do
     curl -s $next_url > last.json
 done
 curl -s $tracks_played_url > $mix_name.json
-cat $mix_name.json | underscore select .track_file_stream_url --outfmt text | xargs -l1 axel
-cat last.json | underscore select .track_file_stream_url --outfmt text | xargs axel
 cat $mix_name.json | underscore select .name --outfmt text > names.txt && cat last.json | underscore select .name --outfmt text >> names.txt
 cat $mix_name.json | underscore select .performer --outfmt text > artists.txt && cat last.json | underscore select .performer --outfmt text >> artists.txt
 cat $mix_name.json | underscore select .release_name --outfmt text > albums.txt && cat last.json | underscore select .release_name --outfmt text >> albums.txt
-cat $mix_name.json  | underscore select .track_file_stream_url --outfmt text | cut -d '/' -f8 > files.txt && cat last.json  | underscore select .track_file_stream_url --outfmt text | cut -d '/' -f8 >> files.txt
-echo "Filename / Song / Artist / Album" > tracklist.txt && paste -d '/' files.txt names.txt artists.txt albums.txt >> tracklist.txt 
+cat $mix_name.json  | underscore select .track_file_stream_url --outfmt text > files.txt && cat last.json  | underscore select .track_file_stream_url --outfmt text >> files.txt
+echo "Filename / Song / Artist / Album" > tracklist.txt && paste -d '|' files.txt names.txt artists.txt albums.txt >> tracklist.txt 
+sed 1d tracklist.txt | while read line
+do
+    axel "`echo $line | cut -d '|' -f1`" -o "`echo $line | cut -d '|' -f2,3,4 --output-delimiter='|'`.m4a"
+done
 rm *.json files.txt names.txt artists.txt albums.txt
 cd ..
